686f80ca9676eae3515e38300cdc2b13fd9433cd
froggleston <robert.davey@earlham.ac.uk>
2017-10-06 17:51:26 +0100

    Fix response when empty pool supplied to avg

--- miso-web/src/main/java/uk/ac/bbsrc/tgac/miso/webapp/controller/rest/PoolRestController.java
+++ miso-web/src/main/java/uk/ac/bbsrc/tgac/miso/webapp/controller/rest/PoolRestController.java
@@ -43,6 +43,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.method.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -298,11 +299,16 @@ public class PoolRestController extends RestController {
     for (Pool p : pools) {
       long sum = 0;
       Set<PoolableElementView> pevs = p.getPoolableElementViews();
-      for (PoolableElementView pev : pevs) {
-        sum += pev.getLibraryDnaSize();
+      if (pevs.size() > 0) {
+        for (PoolableElementView pev : pevs) {
+          sum += pev.getLibraryDnaSize();
+        }
+        long avg = sum / pevs.size();
+        avgs.add(p.getAlias() + ": " + avg);
       }
-      long avg = sum/pevs.size();
-      avgs.add(p.getAlias() + ": " + avg);
+      else {
+        avgs.add(p.getAlias() + ": No elements in pool");
+      }
     }
 
     JSONObject response = new JSONObject();
